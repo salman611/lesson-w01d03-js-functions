72b00fd7e93772be408d15c47e08c80bae4e8c34
Salman Alsuwailem <salman6111@gmail.com>
2020-01-21 17:49:43 +0300

    LAB1 & 2

---
+++ functions.js
@@ -0,0 +1,48 @@
+//LAB 1 (Functions)
+
+//first Q
+
+
+const youRock= function(yourName){
+
+    return ('you rock '+ yourName);
+    
+    };
+//second Q
+const square= function(num){
+
+    return num*num;
+    
+    };
+// Third Q
+
+const cube= function(power){
+
+    return Math.pow(power,3);
+    
+    };
+
+//forth Q
+const toTheFourth= function(power){
+
+    return Math.pow(power,4);
+    
+    };
+
+    //LAB 2 RainDrop Function
+
+    let num;
+num = 1000;
+
+if (num % 3 === 0) {
+    console.log("Pling");
+}
+else if (num % 5 === 0) {
+    console.log("Plang");
+}
+else if (number % 7 === 0) {
+    console.log("Plong");
+} 
+else {
+    console.log(num);
+}
